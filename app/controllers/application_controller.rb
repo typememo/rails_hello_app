6e6c0799d5824a5c285fabfd22edff8d122b99ca
Takeru Yamada <takeru.yamada@rikkyo.ac.jp>
2021-10-19 18:10:15 +0900

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+
+  def hello
+    render html: "hello, world"
+  end
+
+  def hola
+    render html: "¡Hola, mundo"
+  end
+
+  def goodbye
+    render html: "goodbye, world"
+  end
+
+end
